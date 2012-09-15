631ec9b53800f5b5dbff0536bc4db051cf87ba57
jybox <m@jybox.net>
2012-09-15 14:25:30 +0800

    .

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 #RootPanel
 
 ##使用
-###目录挂载
-* `core` -> `/rp/core`
-* `tools` -> `/rp/tools`
-* `static` -> `/rp/web`
-* `panel` -> `/rp/web/panel`
-* LightPHP -> `/rp/web/LightPHP`
-
 
--- static/index.php
+++ static/index.php
@@ -18,7 +18,7 @@ require_once("./config.php");
     <link href="/global.css" rel="stylesheet" type="text/css" />
   </head>
   <body data-spy="scroll" data-target="#rp-sidebar" screen_capture_injected="true">
-    <div class="navbar navbar-fixed-top">
+    <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -26,7 +26,7 @@ require_once("./config.php");
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-          <a class="brand" href="/"></a>
+          <a class="brand" href="/">RP</a>
           <div class="nav-collapse">
             <ul class="nav">
               <li><a href="/">主页</a></li>
