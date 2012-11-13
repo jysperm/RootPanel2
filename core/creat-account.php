18814615932fd193e7a956e55d8e0e916ae6e01a
jybox <m@jybox.net>
2012-11-13 19:54:21 +0800

    默认使用bash

--- core/creat-account.php
+++ core/creat-account.php
@@ -18,7 +18,7 @@ if(!isset($argv[1]))
     die("error : {$argv[0]} <uname>\n");
 $uname=$argv[1];
 
-shell_exec("sudo useradd {$uname} -m");
+shell_exec("sudo useradd {$uname} -m -s/bin/bash");
 shell_exec("sudo usermod -G {$uname} -a www-data");
 
 $conn=new lpMySQL;
