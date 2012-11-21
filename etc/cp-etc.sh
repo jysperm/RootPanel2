466773e0cce8c2fcc2f9ae6f2e9f3d9651c5b6ed
jybox <m@jybox.net>
2012-11-21 23:33:32 +0800

    配置文件

--- etc/cp-etc.sh
+++ etc/cp-etc.sh
@@ -1,11 +1,11 @@
 ﻿#!/bin/bash
 
-cp apache2/apache2.conf /etc/apache2/apache2.conf
-cp apache2/ports.conf /etc/apache2/ports.conf
-cp apache2/sites-enabled/00000-rphost /etc/apache2/sites-enabled/00000-rphost
-cp nginx/nginx.conf /etc/nginx/nginx.conf
-cp nginx/sites-enabled/00000-rphost /etc/nginx/sites-enabled/00000-rphost
-cp php5/cli/conf.d/ming.ini /etc/php5/cli/conf.d/ming.ini
-cp php5/php.ini /etc/php5/apache2/php.ini
-cp php5/php.ini /etc/php5/cgi/php.ini
-cp sudoers /etc/sudoers
+cp /RootPanel/etc/apache2/apache2.conf /etc/apache2/apache2.conf
+cp /RootPanel/etc/apache2/ports.conf /etc/apache2/ports.conf
+cp /RootPanel/etc/apache2/sites-enabled/00000-rphost /etc/apache2/sites-enabled/00000-rphost
+cp /RootPanel/etc/nginx/nginx.conf /etc/nginx/nginx.conf
+cp /RootPanel/etc/nginx/sites-enabled/00000-rphost /etc/nginx/sites-enabled/00000-rphost
+cp /RootPanel/etc/php5/cli/conf.d/ming.ini /etc/php5/cli/conf.d/ming.ini
+cp /RootPanel/etc/php5/php.ini /etc/php5/apache2/php.ini
+cp /RootPanel/etc/php5/php.ini /etc/php5/cgi/php.ini
+cp /RootPanel/etc/sudoers /etc/sudoers
