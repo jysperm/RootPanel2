f48b0239feb958fbda6cdd5b34e052c1913e714a
jybox <m@jybox.net>
2013-06-04 22:48:45 +0800

    提交进度

--- cli/template/apache2.php
+++ cli/template/apache2.php
@@ -4,20 +4,28 @@ $v = $this["hosts"];
 
 ?>
 <VirtualHost *:8080>
+
     ServerName <?= $this["uname"]; ?>
+
     ServerAlias <?= $v["domains"]; ?>
+
     DirectoryIndex <?= $v["general"]["indexs"]; ?>
+
     Options <?= $v["general"]["autoindex"] ? "+" : "-"; ?>Indexes
 
     DocumentRoot <?= $v["source"]; ?>
 
     <? foreach($v["general"]["alias"] as $k => $v): ?>
         Alias <?= $k;?> <?= $v;?>
+
     <? endforeach; ?>
 
     ErrorLog /home/<?= $this["uname"];?>/apache2.access.log
+
     LogLevel warn
+
     CustomLog /home/<?= $this["uname"];?>/apache2.access.log vhost_combined
 
     AssignUserId <?= $this["uname"];?> <?= $this["uname"];?>
+
 </VirtualHost>
