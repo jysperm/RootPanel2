b6224976a7cc673c3df511604363f1e99b723a20
jybox <m@jybox.net>
2013-06-30 00:22:10 +0800

    重构

--- panel/locale/zh_CN/logs.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $rpL;
-
-$rpL["logs.tooltip.logid"] = "每条日志有个唯一的ID, 可以很方便地向客服指明你所说的那条日志";
-$rpL["logs.tooltip.by"] = "该条消息的触发者，可能是你自己，也可能是管理员";
-$rpL["logs.tooltip.ipua"] = "操作者的IP, 悬停鼠标会显示UA";
--- panel/locale/zh_CN/script/locale.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: JyWin7
- * Date: 13-6-8
- * Time: 下午4:36
- * To change this template use File | Settings | File Templates.
- */
