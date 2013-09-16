e88379f63e827c33fc1cddd671ba8d2ee0edb56c
jybox <m@Jybox.net>
2013-09-16 17:02:21 +0800

    Example data for lpPDOModel

--- plugin/UserSystem/model/pUserModel.php
+++ plugin/UserSystem/model/pUserModel.php
@@ -4,14 +4,12 @@ defined("lpInLightPHP") or die(header("HTTP/1.1 403 Not Forbidden"));
 
 class pUserModel extends lpPDOModel
 {
-    protected static $metaData = null;
-
     protected static function metaData($data = null)
     {
         return parent::meta([
             "table" => "user",
             "struct" => [
-                "id" => [self::INT, self::AI],
+                "id" => [self::INT, self::AI, self::PRIMARY],
                 "uname" => [self::VARCHAR => 256],
                 "passwd" => [self::TEXT],
                 "email" => [self::TEXT],
