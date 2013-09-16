81a8a0570b2de375c963b2b4f6b865e8ec6f8912
jybox <m@Jybox.net>
2013-09-16 14:26:02 +0800

    Example data for lpPDOModel

--- plugin/UserSystem/model/pUserModel.php
+++ plugin/UserSystem/model/pUserModel.php
@@ -2,73 +2,24 @@
 
 defined("lpInLightPHP") or die(header("HTTP/1.1 403 Not Forbidden"));
 
-class rpUserModel extends lpPDOModel
+class pUserModel extends lpPDOModel
 {
-    static protected $metaData = null;
+    protected static $metaData = null;
 
-    const NO = "no";
-    const STD = "std";
-    const EXT = "ext";
-    const FREE = "free";
-
-    static protected function metaData()
+    protected static function metaData($data = null)
     {
-        if(!self::$metaData) {
-            self::$metaData = [
-                "db" => f("lpDBDrive"),
-                "table" => "user",
-                "engine" => "MyISAM",
-                "charset" => "utf8",
-                self::PRIMARY => "id"
-            ];
-
-            self::$metaData["struct"] = [
-                "id" => ["type" => self::AI],
-                "uname" => ["type" => self::VARCHAR, "length" => 256],
-                "type" => ["type" => self::VARCHAR, "length" => 256],
-                "passwd" => ["type" => self::TEXT],
-                "email" => ["type" => self::TEXT],
-                "qq" => ["type" => self::TEXT],
-                "settings" => ["type" => self::JSON],
-                "regtime" => ["type" => self::UINT],
-                "expired" => ["type" => self::UINT],
-                "lastlogintime" => ["type" => self::UINT, "default" => 0],
-                "lastloginip" => ["type" => self::TEXT, self::NOTNULL => false],
-                "lastloginua" => ["type" => self::TEXT, self::NOTNULL => false],
-            ];
-
-            foreach(self::$metaData["struct"] as &$v)
-                if(!isset($v[self::NOTNULL]))
-                    $v[self::NOTNULL] = true;
-        }
-
-        return self::$metaData;
-    }
-
-    public function isAllowToPanel()
-    {
-        if($this->data["type"] != self::NO && !$this->isAdmin())
-            return true;
-        else
-            return false;
-    }
-
-    public function isAdmin()
-    {
-        return in_array($this->data["uname"], c("Admins"));
-    }
-
-    public function checkFileName($filename)
-    {
-        $user = $this->data["uname"];
-        $userDir = "/home/{$user}/";
-
-        if(preg_match('%^[/A-Za-z0-9_\-\.]+/?$%', $filename) &&
-            substr($filename, 0, strlen($userDir)) == $userDir &&
-            strlen($filename) < 512  &&
-            substr($filename, -3) != "/.." &&
-            strpos($filename, "/../") === false)
-            return true;
-        return false;
+        return parent::meta([
+            "table" => "user",
+            "struct" => [
+                "id" => [self::INT, self::AI],
+                "uname" => [self::VARCHAR => 256],
+                "passwd" => [self::TEXT],
+                "email" => [self::TEXT],
+                "contacts" => [self::TEXT, self::JSON],
+                "settings" => [self::TEXT, self::JSON],
+                "signup_at" => [self::UINT],
+                "group" => [self::TEXT, self::JSON]
+            ]
+        ]);
     }
 }
