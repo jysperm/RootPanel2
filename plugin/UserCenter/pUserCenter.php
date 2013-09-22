b78c8f7386ad7d55492721d9081c26770feb1adb
jybox <m@Jybox.net>
2013-09-22 10:21:04 +0800

    lpPlugin

--- plugin/UserCenter/pUserCenter.php
+++ plugin/UserCenter/pUserCenter.php
@@ -2,5 +2,20 @@
 
 class pUserCenter extends lpPlugin
 {
+    protected static function metaData()
+    {
+        return parent::meta([
+            "name" => "UserCenter",
+            "version" => ["1.0" => 1],
+            "type" => ["export"],
+            "requestStatic" => [
+                "bootstrap", "jquery"
+            ]
+        ]);
+    }
 
+    protected function init()
+    {
+        $this->load("model/pUserModel");
+    }
 }
