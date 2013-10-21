d84510a162d9ea81a3dd4e33af366766cb94776f
jybox <m@jybox.net>
2013-10-21 16:27:47 +0800

    build tools

--- build/compile-static.php
+++ build/compile-static.php
@@ -1,2 +1,16 @@
 #!/usr/bin/php
 <?php
+
+if(!isset($argc))
+    die("Please run this file in shell.");
+
+define("rpROOT", dirname(__FILE__) . "/..");
+$rpROOT = rpROOT;
+
+$tools = [
+    "cp" => "cp -r"
+];
+
+$baseOutputDir = rpROOT . "/static";
+
+print shell_exec("{$tools["cp"]} '{$rpROOT}/core/source/image' '{$baseOutputDir}/image'");
--- build/install-library.php
+++ build/install-library.php
@@ -1,2 +1,17 @@
 #!/usr/bin/php
 <?php
+
+if(!isset($argc))
+    die("Please run this file in shell.");
+
+define("rpROOT", dirname(__FILE__) . "/..");
+
+require_once(rpROOT . "/LightPHP/LightPHP.php");
+require_once(rpROOT . "/core/include/App.php");
+App::helloWorld();
+
+$tools = [
+    "wget" => "wget"
+];
+
+$baseOutputDir = rpROOT . "/static/library";
