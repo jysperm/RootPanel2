852505e4a568c1f289e31fc30aafbebd74e3145d
jysperm <jysperm@gmail.com>
2014-01-12 05:38:05 +0800

    merge config to one file

--- Core/config/library.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "StaticPrefix" => "/static",
-
-    "bootstrap" => "3.0.3",
-    "jquery" => "2.0.3"
-];
--- Core/config/main.php
+++ Core/config/main.php
@@ -1,9 +1,23 @@
 <?php
 
 return [
+    "DB" => [
+        "type" => "mysql",
+        "host" => "localhost",
+        "dbname" => "rpadmin",
+        "user" => "rpadmin",
+        "passwd" => "passwd",
+        "charset" => "utf8"
+    ],
+
+    "EnablePlugin" => [
+
+    ],
+
     "DefaultLanguage" => "zh_CN",
     "AvailableLanguage" => ["zh_CN"],
 
+    // TODO: 此处应自动检查 /etc/passwd
     "NotAllowSignup" => [
         "root", "default", "admin", "sudo"
     ]
